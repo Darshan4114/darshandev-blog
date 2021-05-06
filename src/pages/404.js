06b077d6d93a8333cae2abf1b07d2f9c4f8f735d
sdfc@koncero.com <sfdc@koncero.com>
2021-05-06 08:50:28 +0530

    initial commit

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 
 import Layout from "@narative/gatsby-theme-novela/src/components/Layout";
@@ -20,34 +19,3 @@ function NotFoundPage() {
 }
 
 export default NotFoundPage;
-=======
-import * as React from "react"
-import { graphql } from "gatsby"
-
-import Layout from "../components/layout"
-import Seo from "../components/seo"
-
-const NotFoundPage = ({ data, location }) => {
-  const siteTitle = data.site.siteMetadata.title
-
-  return (
-    <Layout location={location} title={siteTitle}>
-      <Seo title="404: Not Found" />
-      <h1>404: Not Found</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    </Layout>
-  )
-}
-
-export default NotFoundPage
-
-export const pageQuery = graphql`
-  query {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-  }
-`
->>>>>>> b2269484d0741eadf040860f90379b0acce5e2c2
